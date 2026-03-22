4e13eadc3749ba3ff87b19066bf92f96324a632a
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2026-03-22 11:11:56 +0100

    lab2

--- Lab1/zad5.py
+++ Lab1/zad5.py
@@ -1,1 +1,14 @@
 # 5. Napisz funkcję scalającą (merge) dwie uporządkowane listy w jedną uporządkowana listę.
+
+def merge(arr1, arr2):
+    """
+    Merge two sorted arrays
+    :param arr1:
+    :param arr2:
+    :return:
+    """
+    while len(arr1) > 0 and len(arr2) > 0:
+        if arr1[0] < arr2[0]:
+            arr1.pop(0)
+            arr2[]
+
--- Lab1/zad6.py
+++ Lab1/zad6.py
@@ -1,2 +1,3 @@
 # 6. Napisz funkcję zapisującą do listy liczby w taki sposób, aby w każdym momencie działania
 # programu lista była posortowana.
+
