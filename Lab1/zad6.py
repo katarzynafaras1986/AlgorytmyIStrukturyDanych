637f7c7a1cf18b012827926747ba076a83fae85d
kasia <73136@student.wsiz.edu.pl>
2026-06-24 16:57:10 +0200

    laby1 poprawka

--- Lab1/zad6.py
+++ Lab1/zad6.py
@@ -1,3 +1,21 @@
 # 6. Napisz funkcję zapisującą do listy liczby w taki sposób, aby w każdym momencie działania
 # programu lista była posortowana.
+def insert_sorted(arr, value):
+    i = 0
 
+    while i < len(arr) and arr[i] < value:
+        i += 1
+
+    arr.insert(i, value)
+
+
+if __name__ == "__main__":
+    arr = []
+
+    insert_sorted(arr, 5)
+    insert_sorted(arr, 2)
+    insert_sorted(arr, 8)
+    insert_sorted(arr, 4)
+    insert_sorted(arr, 1)
+
+    print(arr)
