cb5785e96df6dae1f58589e2b41c308cac414e64
kasia <73136@student.wsiz.edu.pl>
2026-06-24 16:55:55 +0200

    laby1 poprawka

--- Lab1/zad5.py
+++ Lab1/zad5.py
@@ -2,13 +2,30 @@
 
 def merge(arr1, arr2):
     """
-    Merge two sorted arrays
-    :param arr1:
-    :param arr2:
-    :return:
+    Merge two sorted arrays.
+
+    :param arr1: pierwsza posortowana lista
+    :param arr2: druga posortowana lista
+    :return: nowa posortowana lista
     """
+
+    result = []
+
     while len(arr1) > 0 and len(arr2) > 0:
         if arr1[0] < arr2[0]:
-            arr1.pop(0)
-            arr2[]
+            result.append(arr1.pop(0))
+        else:
+            result.append(arr2.pop(0))
+
+    while len(arr1) > 0:
+        result.append(arr1.pop(0))
+
+    while len(arr2) > 0:
+        result.append(arr2.pop(0))
+
+    return result
+
+
+if __name__ == "__main__":
+    print(merge([1, 3, 5], [2, 4, 6]))
 
